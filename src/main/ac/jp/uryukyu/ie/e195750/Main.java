95f9330b9940e29b1570e2ebf21ba754e33d4204
Sho Asato <e195750@ie.u-ryukyu.ac.jp>
2019-12-08 17:39:13 +0900

    report4 step3

--- src/main/ac/jp/uryukyu/ie/e195750/Main.java
+++ src/main/ac/jp/uryukyu/ie/e195750/Main.java
@@ -2,15 +2,9 @@ package ac.jp.uryukyu.ie.e195750;
 
 public class Main {
     public static void main(String[] args){
-    try {
-        String str = null;
-        System.out.println(str.length());
-    }
-    catch(NullPointerException e){
-        System.out.println(e+"が発生した。");
-        System.out.println(e.getMessage());
-    }
+        String str = "百二十三";
 
-
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
