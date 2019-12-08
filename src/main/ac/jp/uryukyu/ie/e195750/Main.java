05fe5b882e29983597fff532c4691bdc22f0db8e
Sho Asato <e195750@ie.u-ryukyu.ac.jp>
2019-12-08 17:34:33 +0900

    report4 step1

--- src/main/ac/jp/uryukyu/ie/e195750/Main.java
+++ src/main/ac/jp/uryukyu/ie/e195750/Main.java
@@ -2,8 +2,15 @@ package ac.jp.uryukyu.ie.e195750;
 
 public class Main {
     public static void main(String[] args){
+    try {
         String str = null;
         System.out.println(str.length());
+    }
+    catch(NullPointerException e){
+        System.out.println(e+"が発生した。");
+        System.out.println(e.getMessage());
+    }
+
 
     }
 }
