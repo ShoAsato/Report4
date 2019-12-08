5ffe4c0cd215b3b7b8fd53bc6b91023affc57703
Sho Asato <e195750@ie.u-ryukyu.ac.jp>
2019-12-08 17:25:46 +0900

    report4 step1

---
+++ src/main/ac/jp/uryukyu/ie/e195750/Main.java
@@ -0,0 +1,9 @@
+package ac.jp.uryukyu.ie.e195750;
+
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+
+    }
+}
